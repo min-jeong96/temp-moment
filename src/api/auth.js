acd8130d534a57fcd07bde3bab2534d71712efbd
min-jeong96 <minjeong.dev@gmail.com>
2023-01-19 14:33:57 +0900

    add: login/logout logic with firebase auth

---
+++ src/api/auth.js
@@ -0,0 +1,57 @@
+import { getAuth, signInAnonymously, signOut } from "firebase/auth";
+import { app } from './firebase.js';
+import { getPasswordCode } from './firestore.js';
+import sha256 from 'js-sha256';
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+function signInFirebaseAuth() {
+  return new Promise((resolve, reject) => {
+    signInAnonymously(auth)
+      .then((user) => {
+        resolve(user);
+      })
+      .catch((error) => {
+        reject(new Error(error.message));
+      })
+  });
+}
+
+function signOutFirebaseAuth() {
+  return new Promise((resolve, reject) => {
+    signOut(auth)
+      .then(() => {
+        resolve(true);
+      })
+      .catch((error) => {
+        reject(new Error(error.message));
+      })
+  });
+}
+
+async function login(user, id, password) {
+  let encoded;
+
+  try {
+    encoded = await getPasswordCode(user, id);
+  } catch (error) {
+    // 존재하지 않는 username, moment id
+    console.error(error);
+  }
+
+  if (encoded === sha256(password)) {
+    const user = await signInFirebaseAuth();
+    console.log('login: ', user);
+  } else {
+    // 비밀번호가 일치하지 않는다.
+    console.error('login failed');
+  }
+}
+
+async function logout() {
+  const result = await signOutFirebaseAuth();
+  console.log('logout:', result);
+}
+
+export { auth, login, logout };
